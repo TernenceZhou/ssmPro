905406b79c0edc87aae358da37accb05766a10bb
zhoubangqiang <15213453210@163.com>
2019-06-13 17:24:15 +0800

    2019 0613 优先级队列

--- src/main/test/com/ssm/test/queue/PriorityQueueTest.java
+++ src/main/test/com/ssm/test/queue/PriorityQueueTest.java
@@ -30,21 +30,22 @@ public class PriorityQueueTest {
         }
     }*/
     public static void main(String[] args) {
-        PriorityQueue<Student2> priorityQueue = new PriorityQueue<>();
-        Student2 A = new Student2(98,"A");
-        Student2 B = new Student2(48,"B");
-        Student2 C= new Student2(88,"C");
-        Student2 D = new Student2(99,"D");
+        //student需要 implements Comparable
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+        Student A = new Student(98,"A");
+        Student B = new Student(48,"B");
+        Student C= new Student(88,"C");
+        Student D = new Student(99,"D");
         priorityQueue.add(A);
         priorityQueue.offer(B);
         priorityQueue.offer(C);
         priorityQueue.offer(D);
-        for (Student2 Student2 : priorityQueue) {
+        for (Student Student2 : priorityQueue) {
             System.out.println(Student2.toString());
         }
         System.out.println("优先级队列之后：");
         while (!priorityQueue.isEmpty()){
-            Student2 poll = priorityQueue.poll();
+            Student poll = priorityQueue.poll();
             System.out.println(poll);
         }
     }
