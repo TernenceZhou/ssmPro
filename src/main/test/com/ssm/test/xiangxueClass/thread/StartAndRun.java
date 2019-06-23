1d11e65c8711b82a57b2c74c665db53a1cee27c8
周帮强 <15213453210@163.com>
2019-06-23 10:26:41 +0800

    20190623 thread practice

---
+++ src/main/test/com/ssm/test/xiangxueClass/thread/StartAndRun.java
@@ -0,0 +1,40 @@
+package com.ssm.test.xiangxueClass.thread;
+
+/**
+ * start和run的区别
+ */
+public class StartAndRun {
+
+    private static class UseThread extends Thread{
+        public UseThread(String threadName){
+            super(threadName);
+        }
+        @Override
+        public void run() {
+            int i = 90;
+            while(i>0){
+                SleepTools.ms(1000);
+                System.out.println("I am "+Thread.currentThread().getName()
+                        +" and now the i="+i--);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        UseThread userThread = new UseThread("UseThread");
+        userThread.setName("thread001");
+//        userThread.run(); // 打印结果是i am main 所以表示现在调用的是实例后的一个方法 不是代表thread001线程
+        userThread.start();// 打印结果是i am thread001 所以表示现在调用的是thread001线程
+
+        //Thread.sleep(20);
+        //userThread.interrupt();
+
+        /**
+         * 5 - 10 缺省为5 越小越高
+         * **/
+        userThread.setPriority(5);
+    }
+}
---
+++ src/main/test/com/ssm/test/xiangxueClass/thread/forkjoin/sum/SumNormal.java
@@ -0,0 +1,20 @@
+package com.ssm.test.xiangxueClass.thread.forkjoin.sum;
+
+import com.ssm.test.xiangxueClass.thread.SleepTools;
+
+public class SumNormal {
+	
+	public static void main(String[] args) {
+	    int count = 0;
+	    int[] src = MakeArr.makeArray();
+
+	    long start = System.currentTimeMillis();
+	    for(int i= 0;i<src.length;i++){
+	    	SleepTools.ms(1);
+	    	count = count + src[i];
+	    }
+	    System.out.println("The count is "+ count
+	            +" spend time:"+(System.currentTimeMillis()-start)+"ms");		
+	}
+
+}
