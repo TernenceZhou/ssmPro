885224a90cf88cd0fd395588b3ac8b6a2ffcb579
周帮强 <15213453210@163.com>
2019-08-16 00:03:06 +0800

    位运算

--- src/main/test/com/ssm/test/xiangxueClass/thread/ConcurrentContainer/Permistion.java
+++ src/main/test/com/ssm/test/xiangxueClass/thread/ConcurrentContainer/Permistion.java
@@ -5,5 +5,22 @@ package com.ssm.test.xiangxueClass.thread.ConcurrentContainer;
  */
 public class Permistion {
 
-    private static int PERMISION_SELECT =  1<< 0 ; //0001
+    private static int PERMISION_SELECT =  1<< 0 ; //0001   1
+    private static int PERMISION_INSERT =  1<< 1 ; //0010   2
+    private static int PERMISION_UPDATE =  1<< 2 ; //0100   4
+    private static int PERMISION_DELETE =  1<< 3 ; //1000   8
+    //当前状态
+    int flg = 0;
+
+    public void setPermisionDelete(int per) {
+        flg = per;
+    }
+
+    public  int getPermisionDelete() {
+        return flg;
+    }
+
+    public void removePer(int per) {
+        flg = per;
+    }
 }
