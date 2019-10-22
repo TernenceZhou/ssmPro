703f9468b0ae999938135a9812a597d7f86597c2
zhoubangqiang <15213453210@163.com>
2019-10-22 18:43:50 +0800

    2019 1022 spring

---
+++ src/main/test/com/ssm/xiangxueClass/spring/class02/cap06/config/MyImportBeanDefinitionRegistrar.java
@@ -0,0 +1,17 @@
+package com.ssm.xiangxueClass.spring.class02.cap06.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * @author
+ * @description
+ * @date 2019/10/22
+ */
+public class MyImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry beanDefinitionRegistry) {
+
+    }
+}
