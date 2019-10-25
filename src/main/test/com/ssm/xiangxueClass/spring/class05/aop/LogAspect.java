cb20eee897ef82518252d0742223faff36cd2b42
zhoubangqiang <15213453210@163.com>
2019-10-25 17:47:57 +0800

    2019 1025

--- src/main/test/com/ssm/xiangxueClass/spring/class05/aop/LogAspect.java
+++ src/main/test/com/ssm/xiangxueClass/spring/class05/aop/LogAspect.java
@@ -19,6 +19,16 @@ import java.util.Arrays;
  * @description
  * 日志切面类
  * @date 2019/10/24
+ *
+ * 日志切面类的方法需要动态感知到div()方法运行,
+ *  通知方法:
+ *     前置通知:logStart(); 在我们执行div()除法之前运行(@Before)
+ *     后置通知:logEnd();在我们目标方法div运行结束之后 ,不管有没有异常(@After)
+ *     返回通知:logReturn();在我们的目标方法div正常返回值后运行(@AfterReturning)
+ *     异常通知:logException();在我们的目标方法div出现异常后运行(@AfterThrowing)
+ *     环绕通知:动态代理, 需要手动执行joinPoint.procced()(其实就是执行我们的目标方法div,), 执行之前div()相当于前置通知,
+ *     执行之后就相当于我们后置通知(@Around)
+ *
  */
 @Aspect
 public class LogAspect {
